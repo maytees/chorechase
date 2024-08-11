b23e6ac165867173a240535e70d0a605bd39374b
maytees <mateespublicprofile@gmail.com>
2024-08-11 17:45:19 -0400

    fix: subheading animation hits button

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -8,15 +8,15 @@ import Link from "next/link";
 const Hero = () => {
   return (
     <div className="mt-60 space-y-5 px-5 text-center">
-      <div>
+      <div className="flex flex-col items-center justify-center">
         <BlurIn
           word="chores shouldn't be"
           className="text-6xl text-foreground/85"
         />
         <BlurIn
           word="complicated"
-          duration={2.5}
-          className="animate-pulse text-6xl text-primary"
+          duration={2}
+          className="w-fit animate-pulse text-6xl text-primary underline"
         />
       </div>
       <motion.p
@@ -29,9 +29,15 @@ const Hero = () => {
         <br className="hidden md:inline" />
         to do, for a fake currency, which they can spend in different ways.
       </motion.p>
-      <Button size={"lg"} className="w-96" asChild>
-        <Link href={"/register"}>Get Started</Link>
-      </Button>
+      <motion.div
+        initial={{ opacity: 0, y: 50 }}
+        animate={{ opacity: 0.9, y: 0 }}
+        transition={{ duration: 0.5, delay: 3 }}
+      >
+        <Button size={"lg"} className="w-96" asChild>
+          <Link href={"/register"}>Get Started</Link>
+        </Button>
+      </motion.div>
     </div>
   );
 };
